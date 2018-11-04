6c60c42d001545a49ebdfbe64dc726e4246405ef
xzient <31088051+xzient@users.noreply.github.com>
2018-11-04 07:57:41 -0500

    Add app bar with items. No Sidebar

--- app/src/main/java/cmsc434/fridge1/MainActivity.java
+++ app/src/main/java/cmsc434/fridge1/MainActivity.java
@@ -2,6 +2,11 @@ package cmsc434.fridge1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.app_bar);
+
+        setSupportActionBar(toolbar);
+        setTitle("Lauren");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_user:
+                Toast.makeText(this, "User", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_add:
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_menu:
+                Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return false;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
