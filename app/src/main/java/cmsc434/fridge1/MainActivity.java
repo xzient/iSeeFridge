4cd4c389aa56a3c39c6a4956fe602f5050bf016b
xzient <31088051+xzient@users.noreply.github.com>
2018-11-04 22:07:28 -0500

    Add functional bar and navigation drawer

--- app/src/main/java/cmsc434/fridge1/MainActivity.java
+++ app/src/main/java/cmsc434/fridge1/MainActivity.java
@@ -1,8 +1,13 @@
 package cmsc434.fridge1;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -10,16 +15,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.app_bar);
+        mDrawerLayout = findViewById(R.id.drawer_layout_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
 
         setSupportActionBar(toolbar);
         setTitle("Lauren");
+
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        if (menuItem != null) {
+                            menuItem.setChecked(true);
+                            // close drawer when item is tapped
+                            mDrawerLayout.closeDrawers();
+                        }
+
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+
+
     }
+
 
 
     @Override
@@ -47,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.action_menu:
                 Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show();
+
+                mDrawerLayout.openDrawer(Gravity.END);
                 break;
             default:
                 return false;
@@ -54,4 +90,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
